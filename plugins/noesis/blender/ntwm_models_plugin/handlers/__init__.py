136d797c79bce8a5b73b33839ad0ca9579f9f057
Alexander Evdokimov <alex.ea.li@yandex.ru>
2026-03-02 16:38:17 +0300

    Delete plugins/noesis/blender/ntwm_models_plugin directory

--- plugins/noesis/blender/ntwm_models_plugin/handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .texture_handler import TextureHandler
-from .material_handler import MaterialHandler
-from .animation_handler import AnimationHandler
-
-__all__ = [
-    'TextureHandler',
-    'MaterialHandler',
-    'AnimationHandler',
-]
